649891314b00dccd1b3ad94f7f5d3759c67486de
kn8a <mydreamhomefl@gmail.com>
2022-01-04 11:33:39 -0500

    completed 1st exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    //return array;
+    let toFilter = array;
+    let newArray = toFilter.filter(Value => !args.includes(Value));
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+    let Sum=0;
 
+    if (typeof(low)!='number' || typeof(high) != 'number') {
+        return "ERROR";
+    }
+
+    if (low<0 || high<0) {
+        return "ERROR";
+    }
+    if (low>high) {
+        let temp = low;
+        low = high;
+        high = temp;
+    }
+
+    
+    for (let i=low; i<=high; i++) {
+        Sum = Sum + i;
+    }
+    return Sum;
 };
 
 // Do not edit below this line
