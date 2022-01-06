c3bc3624d754a8b4290f78d4715ddd11d2157c02
kn8a <mydreamhomefl@gmail.com>
2022-01-06 03:27:33 -0500

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return Number(num1) + Number(num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return Number(num1) - Number(num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  //return array;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = array[0]
+  for (let i = 1; i < array.length; i++) {
+    sum = sum * array[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let sum=num1;
+  for (let j = 1; j < num2; j++) {
+    sum = sum * num1;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num==0) {return 1};
+  let sum=num;
+  for (let i=num-1; i>0; i--) {
+    sum = sum * i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
