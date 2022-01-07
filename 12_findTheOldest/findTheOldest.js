e1ce166a09602d0dc367a7bfea4c25fe1ff77a55
kn8a <mydreamhomefl@gmail.com>
2022-01-07 07:29:14 -0500

    completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.sort(function (a,b){ //sort oldest to youngest
+        const firstPerson = getAge(a.yearOfDeath, a.yearOfBirth); //get age of person
+        const nextPerson = getAge(b.yearOfDeath, b.yearOfBirth) //get age of person
+    return nextPerson > firstPerson ? 1 : -1; //compare persons
+    })
+    return people[0];
 };
 
+//function to get age with default of current date if variable not available
+function getAge(death = new Date().getFullYear(), birth) { 
+    return death-birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
