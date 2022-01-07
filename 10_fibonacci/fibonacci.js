c663f2bff33acc14aa595231c3eb3e0b627599da
kn8a <mydreamhomefl@gmail.com>
2022-01-06 23:33:43 -0500

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibLoc) {
+    const index = Number(fibLoc); //convert to integer
+    if (index < 0) {return "OOPS"}; //negative case
+    let fibuArray = [1,1]; //initiate fibonacci array
+    for (let i=2; i<index; i++) { //loop unti desired position
+        num = fibuArray[i-1] + fibuArray[i-2]; //add previous 2 numbers
+        fibuArray.push(num); //add to array
+    }
+    return fibuArray[index-1]; //return position in array
 };
 
 // Do not edit below this line
